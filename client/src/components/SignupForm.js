aaf8e6aadc2f8b8d7af704e71f99646062766715
m1taylor <m1taylor@ryerson.ca>
2023-03-27 16:43:41 -0400

    import mutations to savedbooks

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import { useMutation } from "@apollo/client";
 import { ADD_USER } from "../utils/mutations";
-import { createUser } from "../utils/API";
 import Auth from "../utils/auth";
 
 const SignupForm = () => {
@@ -16,7 +15,7 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
-
+  const [signup, { error, data }] = useMutation(ADD_USER);
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
