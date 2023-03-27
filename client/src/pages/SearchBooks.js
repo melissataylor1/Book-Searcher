012a2c8210179b41b31dbdb42cf4b2c4b7005535
m1taylor <m1taylor@ryerson.ca>
2023-03-27 16:48:26 -0400

    finished search books edit

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -61,14 +61,14 @@ const SearchBooks = () => {
     const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    // const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
-      return false;
-    }
+    // if (!token) {
+    //   return false;
+    // }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({ variables: { bookToSave } });
 
       if (!response.ok) {
         throw new Error("something went wrong!");
@@ -83,11 +83,11 @@ const SearchBooks = () => {
 
   return (
     <>
-      <div fluid className="text-light bg-dark pt-5">
+      <div className="text-light bg-dark p-5">
         <Container>
           <h1>Search for Books!</h1>
           <Form onSubmit={handleFormSubmit}>
-            <Form.Row>
+            <Row>
               <Col xs={12} md={8}>
                 <Form.Control
                   name="searchInput"
@@ -103,13 +103,13 @@ const SearchBooks = () => {
                   Submit Search
                 </Button>
               </Col>
-            </Form.Row>
+            </Row>
           </Form>
         </Container>
       </div>
 
       <Container>
-        <h2>
+        <h2 className="pt-5">
           {searchedBooks.length
             ? `Viewing ${searchedBooks.length} results:`
             : "Search for a book to begin"}
@@ -117,8 +117,8 @@ const SearchBooks = () => {
         <Row>
           {searchedBooks.map((book) => {
             return (
-              <Col md="4">
-                <Card key={book.bookId} border="dark">
+              <Col key={book.bookId} md="4">
+                <Card key={book.bookId} border="dark" id={book.bookId}>
                   {book.image ? (
                     <Card.Img
                       src={book.image}
