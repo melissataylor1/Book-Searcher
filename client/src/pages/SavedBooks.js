da9a34b62a12a897443a6fc0317f5c3246f46f3f
m1taylor <m1taylor@ryerson.ca>
2023-03-27 16:45:01 -0400

    touching up more of saved books, deleting src code that affects api

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,4 +1,3 @@
-import React, { useState, useEffect } from "react";
 import { Container, Card, Button, Row, Col } from "react-bootstrap";
 
 import { useQuery, useMutation } from "@apollo/client";
@@ -7,26 +6,16 @@ import { GET_ME } from "../utils/queries";
 import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
+  // set the query and mutation
   const { loading, data } = useQuery(GET_ME);
   const userData = data?.me || {};
   const [removeBook, _] = useMutation(REMOVE_BOOK);
+
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
-    const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-    if (!token) {
-      return false;
-    }
-
     try {
-      const response = await deleteBook(bookId, token);
+      const { data } = await removeBook({ variables: { bookId } });
 
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
@@ -35,13 +24,13 @@ const SavedBooks = () => {
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
   return (
     <>
-      <div fluid className="text-light bg-dark p-5">
+      <div fluid="true" className="text-light bg-dark p-5">
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
@@ -57,8 +46,8 @@ const SavedBooks = () => {
         <Row>
           {userData.savedBooks.map((book) => {
             return (
-              <Col md="4">
-                <Card key={book.bookId} border="dark">
+              <Col key={book.bookId} md="4">
+                <Card key={book.bookId} border="dark" id={book.bookId}>
                   {book.image ? (
                     <Card.Img
                       src={book.image}
