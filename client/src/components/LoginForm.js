8d91089894e5011b8633a0fa9bba5524f9751ead
m1taylor <m1taylor@ryerson.ca>
2023-03-27 16:39:33 -0400

    adding mutations and imports to login and signup

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,7 +2,6 @@
 import React, { useState } from "react";
 import { Form, Button, Alert } from "react-bootstrap";
 import { useMutation } from "@apollo/client";
-
 import { LOGIN_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
 
@@ -25,19 +24,10 @@ const LoginForm = () => {
       event.preventDefault();
       event.stopPropagation();
     }
-
     try {
-      const response = await LOGIN_USER(userFormData);
       const { data } = await login({ variables: { ...userFormData } });
 
       Auth.login(data.login.token);
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
